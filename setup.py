70dd449bbe614beb54acaba7de623c718e30b6bd
Davi Ortega <daviortega@gmail.com>
2016-12-23 13:38:20 -0800

    Fixing test setup in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -14,8 +15,18 @@ requirements = [
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'pytest'
 ]
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
 
 setup(
     name='bitkPy',
@@ -24,7 +35,7 @@ setup(
     long_description=readme + '\n\n' + history,
     author="BioWonks",
     author_email='ortegad@caltech.edu',
-    url='https://github.com/daviortega/bitkPy',
+    url='https://github.com/biowonks/bitkPy',
     packages=[
         'bitkPy',
     ],
@@ -40,14 +51,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=test_requirements,
+    cmdclass={'test': PyTest}
 )
